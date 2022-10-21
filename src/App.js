5a24149902e60238c299370e7616f5799917c82c
Vovchenko Olga <vovchenkoolga@gmail.com>
2022-10-21 21:59:17 +0300

    first release

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import Child from './Child'
+import React, {useState, useCallback} from 'react';
+
+function App() {
+  const[ran,setRan] = useState(0);
+     const handleClick=useCallback(()=>setRan(Math.floor(Math.random()*11)+1),[]);
+    return (
+      <body>
+    <div className="App">
+      <header className="App-header">
+        <h1>
+        Гадание по книгам. Получи ответ свой на вопрос и совет на будущее. 
+        </h1>
+      </header>
+      <button onClick={()=> handleClick()}>Нажми и ты получишь ответ</button>
+      <div className="Answer">
+      <Child new={ran} />
+      </div>
+    </div>
+    </body>
+  );
+}
+
+export default App;
