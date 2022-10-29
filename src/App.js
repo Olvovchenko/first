e96575f0cb8b6134385fc93df8ff0fd1166fe783
Vovchenko Olga <vovchenkoolga@gmail.com>
2022-10-29 21:04:18 +0300

    no more that 5 question

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ function App() {
   const count = useRef(0);
   useEffect(() =>{count.current = count.current+1;});
 
-     const handleClick=useCallback(()=>setRan(Math.floor(Math.random()*11)+1),[]);
+     const handleClick=useCallback(()=>setRan(count.current<5?Math.floor(Math.random()*11)+1:12),[]);
     return (
       <body>
     <div className="App">
       <header className="App-header">
         <h2>
-        Гадание по книгам. Получи ответ свой на вопрос и совет на будущее.Вы нажали {count.current} раз. 
+        Гадание по книгам. Получи ответ свой на вопрос и совет на будущее. 
         </h2>
       </header>
       <button onClick={()=> handleClick()}>Нажми и ты получишь ответ</button>
