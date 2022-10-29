b622b0d8860dc552b557ff9f08f362be425e5758
Vovchenko Olga <vovchenkoolga@gmail.com>
2022-10-29 20:51:05 +0300

    useRef on h(you can see how you click)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import './App.css';
 import Child from './Child'
-import React, {useState, useCallback} from 'react';
+import React, {useState, useCallback, useRef, useEffect} from 'react';
 
 function App() {
   const[ran,setRan] = useState(0);
+  const count = useRef(0);
+  useEffect(() =>{count.current = count.current+1;});
+
      const handleClick=useCallback(()=>setRan(Math.floor(Math.random()*11)+1),[]);
     return (
       <body>
     <div className="App">
       <header className="App-header">
         <h2>
-        Гадание по книгам. Получи ответ свой на вопрос и совет на будущее. 
+        Гадание по книгам. Получи ответ свой на вопрос и совет на будущее.Вы нажали {count.current} раз. 
         </h2>
       </header>
       <button onClick={()=> handleClick()}>Нажми и ты получишь ответ</button>
