1ce1513eef320755c1db7db7039a629194803915
Vovchenko Olga <vovchenkoolga@gmail.com>
2022-10-31 00:50:38 +0200

    useRef change background+comment

--- src/App.js
+++ src/App.js
@@ -2,14 +2,30 @@ import './App.css';
 import Child from './Child'
 import React, {useState, useCallback, useRef, useEffect} from 'react';
 
+
+
 function App() {
   const[ran,setRan] = useState(0);
   const count = useRef(0);
-  useEffect(() =>{count.current = count.current+1;});
+  useEffect(() =>{
+    count.current = count.current+1;
+    });
 
-     const handleClick=useCallback(()=>setRan(count.current<5?Math.floor(Math.random()*11)+1:12),[]);
+  /*useEffect(() =>{
+    count.current % 2 === 0 ? 
+    document.body.classList.add('AP1') : 
+    document.body.classList.remove('AP1');
+    console.log (count.current);
+  },[count.current]);*/
+
+  const handleClick=useCallback(()=>{
+  console.log (count.current)
+  setRan(count.current<5?Math.floor(Math.random()*11)+1:12);
+  },
+  [count]);
     return (
-      <body>
+    
+     <div>
     <div className="App">
       <header className="App-header">
         <h2>
@@ -21,7 +37,7 @@ function App() {
       <Child new={ran} />
       </div>
     </div>
-    </body>
+    </div>
   );
 }
 
