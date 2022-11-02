27e973e9e5fcf775fe41d76de43dd7f1d256c263
Vovchenko Olga <vovchenkoolga@gmail.com>
2022-11-02 23:32:57 +0200

    Button component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import './App.css';
-import Child from './Child'
+import Child from './Child';
+import ButStyle from './Btn';
 import React, {useState, useCallback, useRef, useEffect} from 'react';
 
 
 
 function App() {
   const[ran,setRan] = useState(0);
+  const[value,setValue]=useState('jkz');
   const count = useRef(0);
   useEffect(() =>{
     count.current = count.current+1;
-    });
+    },[ran]);
 
-  /*useEffect(() =>{
+  useEffect(() =>{
     count.current % 2 === 0 ? 
     document.body.classList.add('AP1') : 
     document.body.classList.remove('AP1');
-    console.log (count.current);
-  },[count.current]);*/
+    },[ran]);
 
   const handleClick=useCallback(()=>{
-  console.log (count.current)
+  console.log (count.current);
   setRan(count.current<5?Math.floor(Math.random()*11)+1:12);
   },
   [count]);
@@ -32,12 +33,20 @@ function App() {
         Гадание по книгам. Получи ответ свой на вопрос и совет на будущее. 
         </h2>
       </header>
+      <form>
+      <h3 style={{color:'lightcoral'}}> Введите, пожалуйста свое имя</h3>
+      <input type="text" 
+      value={value}
+      onChange = {event => setValue(event.target.value)}
+      ></input>
+      </form>
       <button onClick={()=> handleClick()}>Нажми и ты получишь ответ</button>
+      <ButStyle>Подробности...</ButStyle>
       <div className="Answer">
       <Child new={ran} />
       </div>
-    </div>
-    </div>
+      </div>
+      </div>
   );
 }
 
