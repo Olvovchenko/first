67e5ea5f283f014154473def39b1538db3b74bf0
Vovchenko Olga <vovchenkoolga@gmail.com>
2022-11-03 01:03:26 +0200

    ref plus focus

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ import React, {useState, useCallback, useRef, useEffect} from 'react';
 
 function App() {
   const[ran,setRan] = useState(0);
-  const[value,setValue]=useState('jkz');
+  const[value1,setValue1]=useState('');
+  const[value2,setValue2]=useState('');
+  
   const count = useRef(0);
   useEffect(() =>{
     count.current = count.current+1;
@@ -19,11 +21,18 @@ function App() {
     document.body.classList.remove('AP1');
     },[ran]);
 
+    const znakInputRef = useRef();
+
   const handleClick=useCallback(()=>{
   console.log (count.current);
   setRan(count.current<5?Math.floor(Math.random()*11)+1:12);
   },
   [count]);
+  
+  const handlerKeyUp = (e) => {
+    if (e.key==="Enter") {znakInputRef.current.focus();
+    }
+  };
     return (
     
      <div>
@@ -36,11 +45,22 @@ function App() {
       <form>
       <h3 style={{color:'lightcoral'}}> Введите, пожалуйста свое имя</h3>
       <input type="text" 
-      value={value}
-      onChange = {event => setValue(event.target.value)}
+      value={value1}
+      placeholder="Полное имя"
+      onChange = {event => setValue1(event.target.value)}
+      onKeyUp = {handlerKeyUp}
       ></input>
+      <input 
+      type="text"
+      value ={value2}
+      ref={znakInputRef}
+      placeholder='ваш знак зодиака'
+      onChange= {event => setValue2(event.target.value)}
+      ></input>
+
       </form>
       <button onClick={()=> handleClick()}>Нажми и ты получишь ответ</button>
+      <br />
       <ButStyle>Подробности...</ButStyle>
       <div className="Answer">
       <Child new={ran} />
